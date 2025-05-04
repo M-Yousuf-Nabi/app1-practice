aa98d4659ce1cd489c5109e7b07cd98bc3440e88
M Yousuf <mdyousuf35.bd@gmail.com>
2025-05-04 13:47:28 +0600

    Module 07 done here

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,103 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-  IntroApp()
-  );
+  runApp(IntroApp());
 }
 
-class IntroApp extends StatelessWidget{
+class IntroApp extends StatelessWidget {
+  const IntroApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
-        home: Scaffold(
-          appBar: AppBar(
-            backgroundColor: Colors.blueAccent,
-            title: Text('Menu'),
-          ),
-          body: Center(
-            child: Text("Hello World",
-              textAlign: TextAlign.justify,
-              style: TextStyle(
-              fontSize: 30,
-              fontWeight: FontWeight.bold,
-              decoration: TextDecoration.underline,
-              letterSpacing: 10,
-              wordSpacing: 10,
-                  overflow: TextOverflow.ellipsis,
-            ),
-            ),
-          ),
-        )
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Intro App",
+      home: Home(),
     );
   }
-  
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.teal,
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: Text("Home"),
+      ),
+      body: Center(
+        // child: Image.asset(
+        //   "assets/Image/01.jpeg",
+        //   width: 300,
+        //   height: 400,
+        //   fit: BoxFit.cover,
+        // ),
+
+        // Icon(
+        //   Icons.android,
+        //   size: 100,
+        //   color: Colors.white,
+        // ),
+        // Text("Hello",
+        // style: TextStyle(
+        //   fontSize: 30,
+        //   fontWeight: FontWeight.bold,
+        // ),),
+
+        // child: ElevatedButton(
+        //     onPressed: () {
+        //       print("Button pressed");
+        //     },
+        //
+        //     child: Text("Notification",
+        //     style: TextStyle(
+        //       fontSize: 40,
+        //       fontStyle: FontStyle.italic,
+        //       color: Colors.black,
+        //     ),
+        //     )
+        // ),
+
+        // child: IconButton(onPressed: () {
+        //   print("Add");
+        // },
+        //  icon: Icon(Icons.add),
+        // ),
+
+        // child: TextButton(
+        //   onPressed: () {
+        //     print("Show button pressed");
+        //   },
+        //   child: Text("Text Button"),
+        // ),
+
+        // child: GestureDetector(
+        //   onTap: (){
+        //     print("Single Tapped");
+        //   },
+        //   onDoubleTap: (){
+        //     print("Double Tapped");
+        //   },
+        //   child: Image.asset(
+        //     "assets/Image/01.jpeg",
+        //     width: 300,
+        //     height: 400,
+        //     fit: BoxFit.cover,
+        //   ),
+
+       //  child: InkWell(
+       //     onTap: (){
+       //        print("Single Tapped");
+       //  },
+       //  onDoubleTap: (){
+       //        print("Double Tapped");
+       //  },
+       //  child: Text("Hello World"),
+       // ),
+      ),
+    );
+  }
 }
