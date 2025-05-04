865eb201f6ee0654a63c71460f16e0189fd66922
M Yousuf <mdyousuf35.bd@gmail.com>
2025-05-04 15:01:48 +0600

    Module 8 start from here

--- lib/main.dart
+++ lib/main.dart
@@ -23,81 +23,23 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.teal,
-      appBar: AppBar(
-        backgroundColor: Colors.blue,
-        title: Text("Home"),
-      ),
-      body: Center(
-        // child: Image.asset(
-        //   "assets/Image/01.jpeg",
-        //   width: 300,
-        //   height: 400,
-        //   fit: BoxFit.cover,
-        // ),
-
-        // Icon(
-        //   Icons.android,
-        //   size: 100,
-        //   color: Colors.white,
-        // ),
-        // Text("Hello",
-        // style: TextStyle(
-        //   fontSize: 30,
-        //   fontWeight: FontWeight.bold,
-        // ),),
-
-        // child: ElevatedButton(
-        //     onPressed: () {
-        //       print("Button pressed");
-        //     },
-        //
-        //     child: Text("Notification",
-        //     style: TextStyle(
-        //       fontSize: 40,
-        //       fontStyle: FontStyle.italic,
-        //       color: Colors.black,
-        //     ),
-        //     )
-        // ),
-
-        // child: IconButton(onPressed: () {
-        //   print("Add");
-        // },
-        //  icon: Icon(Icons.add),
-        // ),
-
-        // child: TextButton(
-        //   onPressed: () {
-        //     print("Show button pressed");
-        //   },
-        //   child: Text("Text Button"),
-        // ),
-
-        // child: GestureDetector(
-        //   onTap: (){
-        //     print("Single Tapped");
-        //   },
-        //   onDoubleTap: (){
-        //     print("Double Tapped");
-        //   },
-        //   child: Image.asset(
-        //     "assets/Image/01.jpeg",
-        //     width: 300,
-        //     height: 400,
-        //     fit: BoxFit.cover,
-        //   ),
-        // )
-       //  child: InkWell(
-       //     onTap: (){
-       //        print("Single Tapped");
-       //  },
-       //  onDoubleTap: (){
-       //        print("Double Tapped");
-       //  },
-       //  child: Text("Hello World"),
-       // ),
-      ),
+        backgroundColor: Colors.teal,
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          title: Text("Home"),
+        ),
+        body: Column(children: [
+          Text("Muhammed"),
+          Text("Yousuf"),
+          Text("Nabi"),
+          ElevatedButton(
+            onPressed: () {
+              print("Button Clicked");
+            },
+            child: Text("Click Here"),
+          )
+        ]
+      )
     );
   }
 }
