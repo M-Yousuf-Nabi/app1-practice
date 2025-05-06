e9837f54715055594a3592089d55c4bbedcc0a9e
M Yousuf <mdyousuf35.bd@gmail.com>
2025-05-06 19:04:27 +0600

    Module 8-02 Start from here

--- lib/main.dart
+++ lib/main.dart
@@ -23,127 +23,84 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.teal,
-        appBar: AppBar(
-          backgroundColor: Colors.blue,
-          title: Text("Home"),
-        ),
-        body: SingleChildScrollView(
-          scrollDirection: Axis.horizontal,
-            child: Row(
-             // mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Column(
-                  children: [
-                    Text("Column 1"),
-                    Text("Column 1"),
-                    Text("Column 1"),
-                    Text("Column 1"),
-                    Text("Column 1"),
-                    Text("Column 1"),
-                    Text("Column 1"),
-                    Text("Column 1"),
-                    Text("Column 1"),
-                    Row(
-                      children: [
-                        Text("Comumn 1 Row 1"),
-                        Text("Comumn 1 Row 1"),
-                        Text("Comumn 1 Row 1"),
-                        Text("Comumn 1 Row 1"),
-                        Text("Comumn 1 Row 1"),
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: Text("Menu"),
+      ),
+      // body:  ListView(
+      //   scrollDirection: Axis.vertical,
+      //   children: [
+      //     SizedBox(
+      //       height: 150,
+      //       width: 150,
+      //       child: Center(
+      //         child: Text("Box 1"),
+      //       )
+      //     ),
+      //     SizedBox(
+      //         height: 200,
+      //         width: 200,
+      //         child: Center(
+      //           child: Text("Box 1"),
+      //         )
+      //     ),
+      //     SizedBox(
+      //         height: 200,
+      //         width: 200,
+      //         child: Center(
+      //           child: Text("Box 1"),
+      //         )
+      //     ),
+      //     SizedBox(
+      //         height: 200,
+      //         width: 200,
+      //         child: Center(
+      //           child: Text("Box 1"),
+      //         )
+      //     ),SizedBox(
+      //         height: 150,
+      //         width: 150,
+      //         child: Center(
+      //           child: Text("Box 1"),
+      //         )
+      //     ),
+      //     SizedBox(
+      //         height: 200,
+      //         width: 200,
+      //         child: Center(
+      //           child: Text("Box 1"),
+      //         )
+      //     ),
+      //     SizedBox(
+      //         height: 200,
+      //         width: 200,
+      //         child: Center(
+      //           child: Text("Box 1"),
+      //         )
+      //     ),
+      //     SizedBox(
+      //         height: 200,
+      //         width: 200,
+      //         child: Center(
+      //           child: Text("Box 1"),
+      //         )
+      //     )
+      //   ],
 
-                      ],
-                    )
-                  ],
-                ),
-                Column(
-                  children: [
-                    Text("Column 2"),
-                    Text("Column 2"),
-                    Text("Column 2"),
-                    Text("Column 2"),
-                    Text("Column 2"),
-                    Text("Column 2"),
-                    Text("Column 2"),
-                    Text("Column 2"),
-                    Text("Column 2"),
-                  ],
-                ),
-                Column(
-                  children: [
-                    Text("Column 3"),
-                    Text("Column 3"),
-                    Text("Column 3"),
-                    Text("Column 3"),
-                    Text("Column 3"),
-                    Text("Column 3"),
-                    Text("Column 3"),
-                    Text("Column 3"),
-                    Text("Column 3"),
-
-                  ],
-                ),
-                Column(
-                  children: [
-                    Text("Column 4"),
-                    Text("Column 4"),
-                    Text("Column 4"),
-                    Text("Column 4"),
-                    Text("Column 4"),
-                    Text("Column 4"),
-                    Text("Column 4"),
-                    Text("Column 4"),
-                    Text("Column 4"),
-                  ],
-                )
-              ],
-            )
-        )
-
-
-         // SingleChildScrollView(
-         //   scrollDirection: Axis.horizontal,
-         //   child:  Row(
-         //     mainAxisSize: MainAxisSize.max,
-         //     mainAxisAlignment: MainAxisAlignment.spaceAround,
-         //     children: [
-         //       Text("Lorem Ipsum is simply dummy text of the printing and typesetting industry. "
-         //           "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"
-         //           ", when an unknown printer took a galley of type and scrambled it t"
-         //           "o make a type specimen book. It has survived not only five centuri"
-         //           "es, but also the leap into electronic typesetting, remaining essentially "
-         //           "unchanged. It was popularised in the 1960s with the release of Letraset"
-         //           " sheets containing Lorem Ipsum passages, and more recently with desktop "
-         //           "publishing software like Aldus PageMaker including versions of Lorem "
-         //           "Ipsum. "),
-         //       Text("Yousuf "),
-         //
-         //       Text("Nabi"),
-         //     ],
-         //   ),
-         // )
+      body: ListView.builder(
+        itemCount: 1000,
+        itemBuilder: (context, index){
+          return SizedBox(
+            width: 100,
+            height: 100,
+            child: Text(index.toString()),
+          );
+        },
+      ),
 
 
-        // Column(
-        //    mainAxisSize: MainAxisSize.max,
-        //    mainAxisAlignment: MainAxisAlignment.spaceAround,
-        //    crossAxisAlignment: CrossAxisAlignment.center  ,
-        //     children: [
-        //       Text("Muhammed"),
-        //       Text("Yousuf"),
-        //       Text("Nabi"),
-        //       ElevatedButton(
-        //         onPressed: () {
-        //           print("Button Clicked");
-        //         },
-        //         child: Text("Click Here"),
-        //       ),
-        //       IconButton(
-        //         onPressed: () {},
-        //         icon: Icon(Icons.access_alarm)
-        //       )
-        //     ]
-        // )
+
     );
   }
 }
